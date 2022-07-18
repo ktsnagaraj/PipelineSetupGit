de8a2fae0a336013416a7e2e1e1d7c6653f8dc8c
Dra Nallely Corona <Dra Nallely Corona@LAPTOP-IDB7RV6C>
2022-07-17 19:57:52 -0500

    Initial GitHub Setup

---
+++ PipelineSetup/src/test/java/StepDefinition/SearchAProduct.java
@@ -0,0 +1,26 @@
+package StepDefinition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class SearchAProduct {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		Thread.sleep(2000);
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dra Nallely Corona\\Downloads\\NewChromeDriver\\chromedriver.exe");
+		Thread.sleep(2000);
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com.mx/");
+		Actions action = new Actions(driver);
+		Thread.sleep(2000);
+		WebElement prime = driver.findElement(By.xpath("//span[text()='Prime']"));
+		action.moveToElement(prime);
+		Thread.sleep(2000);
+		driver.quit();
+	}
+}
